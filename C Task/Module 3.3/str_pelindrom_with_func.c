22c43179f50c54afafb99284bbab814b38607661
Ashok153 <baldaniyaashok153@gmail.com>
2022-08-15 14:35:03 +0530

    string

--- C Task/Module 3.3/str_pelindrom_with_func.c
+++ C Task/Module 3.3/str_pelindrom_with_func.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+void pel(char s[1000])
 {
     int i, flag = 0;
-    char s[100] = "hello world dlrow olleh";
     int l = strlen(s);
     for (i = 0; i < l; i++)
     {
         if (s[i] != s[l - i - 1])
         {
             flag = 1;
+            break;
         }
     }
     if (flag == 0)
@@ -21,6 +21,10 @@ int main()
     {
         printf("string is not pelindrome");
     }
+}
 
+int main()
+{
+    pel("hello world");
     return 0;
 }
